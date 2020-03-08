4156af82e170ec88dab0051adabfdde4f8b7e657
jch_glb <1623873393@qq.com>
2020-03-08 22:25:35 +0800

    22222second

---
+++ src/main/java/top/labos/git/TestGitSecond.java
@@ -0,0 +1,17 @@
+package top.labos.git;
+
+public class TestGitSecond {
+    public static void main(String[] args) {
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+    }
+    public void test(){
+        System.out.println("sadfkhaslfjh");
+        System.out.println("sadfkhaslfjh");
+        System.out.println("sadfkhaslfjh");
+    }
+}
