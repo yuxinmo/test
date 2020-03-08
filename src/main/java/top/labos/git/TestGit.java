576f77b2ffadcd4e50cf18eecfec184705cfb6e2
jch_glb <1623873393@qq.com>
2020-03-08 21:02:40 +0800

    testGit

---
+++ src/main/java/top/labos/git/TestGit.java
@@ -0,0 +1,12 @@
+package top.labos.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+    }
+}
