0f92d3f53538b5ec9da6c4a32a96fb0e8c61fc7e
jch_glb <1623873393@qq.com>
2020-03-09 11:42:14 +0800

    测试忽略文件2

--- src/main/java/top/labos/git/TestGit.java
+++ src/main/java/top/labos/git/TestGit.java
@@ -31,5 +31,7 @@ public class TestGit {
         System.out.println("sadfkhaslfjh");
         System.out.println("sadfkhaslfjh");
         System.out.println("我是最新修改的，在GitHub里修改的2");
+        System.out.println("我是最新修改的，在GitHub里修改的2");
+
     }
 }
