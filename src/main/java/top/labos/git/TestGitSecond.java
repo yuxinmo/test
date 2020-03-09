cc948f1c0cbecfea5e7f6c8da82ff1014d8fd3ff
jch_glb <1623873393@qq.com>
2020-03-09 12:59:52 +0800

    测试忽略文件2

--- src/main/java/top/labos/git/TestGitSecond.java
+++ src/main/java/top/labos/git/TestGitSecond.java
@@ -17,8 +17,15 @@ public class TestGitSecond {
     public void info(){
         System.out.println("先update project（pull），再push");
         System.out.println("的身份根深蒂固山豆根");
-        System.out.println("sadfkhaslfjh");
+        System.out.println("修改了");
         System.out.println("我是再gitTest里面的");
+    }
+
+    public void tryInfo(){
+        System.out.println("编写代码");
+        System.out.println("commit");
+        System.out.println("然后pull");
+        System.out.println("我是在idea里面的");
     }
 
 }
