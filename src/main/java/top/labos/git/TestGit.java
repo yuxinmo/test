f6d1b23122aee53cfa68d4ec795d8e5a8fc2b2c2
jch_glb <1623873393@qq.com>
2020-03-09 11:11:09 +0800

    测试忽略文件2

--- src/main/java/top/labos/git/TestGit.java
+++ src/main/java/top/labos/git/TestGit.java
@@ -1,5 +1,15 @@
 package top.labos.git;
 
+/**
+ * 测试
+ * 注释：
+ * .gitignore和exclude区别：
+ *      .gitignore：需要提交的Git仓库，多用于忽略公共性的文件，例：java产生的.classpath | .project | logs/ ...；
+ *      exclude：不需要提交Git仓库，用户个人配置文件或某些测试类；
+ * .iml文件和.idea文件夹：
+ *      .iml是 intellij idea的工程配置文件，里面是当前project的一些配置信息。
+ *      .idea存放项目的配置信息，包括历史记录，版本控制信息等。
+ */
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git project!");
